2287018be62dcec1f2071f3bc04d47984a74215d
Fabien Berthoux <thoux08@gmail.com>
2021-01-19 20:06:49 +0100

    Add validations to Restaurant and Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  RATING = [0,1,2,3,4,5]
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }, inclusion: { in: RATING }
 end
